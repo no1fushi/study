f7d1d6447afe05124215f4f193e7a946196397b3
no1fushi <no1fushi@gmai.com>
2014-09-16 17:49:54 +0900

    勉強

---
+++ Ruby/Method.rb
@@ -0,0 +1,9 @@
+begin
+  オブジェクト=データ型
+  メソッド=命令
+  クラス=メソッドを定義したもの
+end
+
+Method = "Hello World"
+
+p Method.length #lengthメソッド=文字列の長さを返すメソッド
---
+++ Ruby/Variable.rb
@@ -0,0 +1,8 @@
+Variable="Value" #変数
+
+p Variable #変数を出力
+
+
+ADMIN_EMAIL ="notchange"#変更したくない変数
+
+ADMIN_EMAIL ="change"#変更できずエラーが出る
