2d9d9f414309821ce3b8e5dbecaad3122f178545
no1fushi <no1fushi@gmai.com>
2014-10-14 10:14:17 +0900

    JavaScript基礎

---
+++ JavaScript/Variable.js
@@ -0,0 +1,17 @@
+/*変数-宣言をしないといけない
+
+JavaScriptで用いられるデータ型
+文字列
+数値
+真偽値（true / false）
+オブジェクト（配列、関数、組み込みオブジェクト）
+undefined-定義されていない
+null-何もない*/
+
+	var msg;//宣言する
+		msg = "HelloWorld";//右辺のものを左辺に代入する
+
+	console.log(msg);
+
+/*var msg ="HelloWolrd"と一気に書くことも可能
+また続けて変数を書くことも可能*/
