849cf0d384f286ffda7c2ef5bd34081c60334bc0
no1fushi <no1fushi@gmai.com>
2014-11-01 22:38:59 +0900

    変数演算

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -4,11 +4,11 @@
 
 #include <stdio.h>//Standard input outputというヘッダーファイルを読み込む
 
-main(void){
+int main(void){
 
 	puts("Hello World");//１つの命令が終わったら;をつける
 
-return 0;
+return 0;//無事終わったら0を返す
 }
 
 /*実行結果"Hello World"と表示される
