1a59b57bc03f0feed8bfe884ba424dcfdf22fd59
Katchan <no1fushi@live.jp>
2016-04-23 23:47:46 +0900

    jyanken 一応完成形?

--- C/proken/jyanken.cpp
+++ C/proken/jyanken.cpp
@@ -2,16 +2,19 @@
 //
 #include <stdio.h>
 #include <stdlib.h>//rand()入りヘッダ
+#include <time.h>//time入りヘッダ
 
 //ランダム関数
 int randum(void){//hの命令と関数名はかぶってはいけない
 	int par;
+	srand((unsigned)time(NULL));
 	par = (rand() % 3 ) + 1;//3で割って余りを求めて1を+(0,1,2,3)なので
+	printf("%d\n" ,par);
 	return par;//parを返す
 }
 
 //勝ち負け判定
-void gudge(int player, int com){
+int gudge(int player, int com){
 
 	/*勝ちパターン 負けパターン
 	player com     player com
@@ -21,16 +24,34 @@ void gudge(int player, int com){
 	*/
 	if((player == 1 || player == 2) && (player == com-1)){
 		printf("あなたの勝ちいー おめでとう笑\n");
+		return 1;
 	}else if(player == 3 && com == 1){
 		printf("あなたの勝ちいー おめでとう笑\n");
+		return 1;
 	}else if(player == 1 && com == 3){
 		printf("俺の勝ちいいあなたの負けええ\n");
+		return 2;
 	}else if((player == 2 || player == 3) && (player == com+1)){
 		printf("俺の勝ちいいあなたの負けええ\n");
+		return 2;
 	}else if(player == com){
 		printf("あいこだよーん");
+		return 0;
 	}else{
 		printf("ずるはダメだよ");
+		return 0;
+	}
+}
+
+//
+void hand(int somehand){
+	switch(somehand){
+		case 1: printf("グー");
+			break;
+		case 2: printf("ちょき");
+			break;
+		case 3: printf("ぱー");
+			break;
 	}
 }
 
@@ -38,17 +59,37 @@ int main(void) {
 
 	int x;//デバック用
 	int myhand, cpuhand;
+	int mywin,  cpuwin;
+	cpuwin = 0;
+	mywin = 0;
+
 
 	printf("じゃんけんしようぜ!\n");
 	printf("数字の1が[グー]2が[ちょき]3が[パー]だぜ\n");
-	printf("じゃあああんけえええん...\n");
+	while(mywin < 3 && cpuwin < 3){
+		printf("じゃあああんけえええん...\n");
 
-	scanf("%d" , &myhand);
-	//cpuhand = 1;必ずぐー
+		scanf("%d" , &myhand);
+		//cpuhand = 1;必ずぐー
 
-	cpuhand = randum();
+		cpuhand = randum();
 
-	gudge(myhand,cpuhand);
+		hand(myhand);
+		printf("対");
+		hand(cpuhand);
+		printf("\n");
+
+		x = gudge(myhand,cpuhand);
+
+		switch(x){
+		case 1: mywin++;
+			break;
+		case 2: cpuwin++;
+			break;
+		}
+
+		printf("%d 対 %d\n" , mywin,cpuwin);
+	}
 
 	/*ぐーだけの場合
 	if(myhand == 1){
@@ -60,6 +101,14 @@ int main(void) {
 	}else{
 		printf("ずるはダメだよ");
 	}*/
+
+	if(myhand == 3){
+		printf("YOU WIN!");
+	}else if(cpuwin == 3){
+		printf("YOU LOSET");
+	}else{
+		printf("ERROR\n");
+	}
 
 	scanf("%d" , &x);//デバック用
 
