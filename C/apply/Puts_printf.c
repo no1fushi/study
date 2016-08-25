cd677392bdd1391c649d6a4800fbe434c90ab7ee
Katchan <no1fushi@live.jp>
2016-08-26 03:06:46 +0900

    move Puts_Printf.c,Puts_Printf.exe

--- C/apply/Puts_printf.c
+++ C/apply/Puts_printf.c
@@ -1,4 +1,4 @@
-//puts関数とprintf関数の違い
+//puts関数とprintf関数の違い、使い方
 #include <stdio.h>
 
 int main(void){
@@ -12,13 +12,21 @@ int main(void){
 	printf("C言語プログラミングの\n");
 	printf("勉強");
 
+	int a = 2;
+	printf("\naは%d\n",a);
+	printf("a+2は%d\n",a+2);//こういったこともできる
+	printf("1+2は%d\n",1+2);
+
 	return 0;
 }
 
 /*実行結果 デフォルトではputs関数は改行して
  表示されprintf関数は改行されない
+ aは2,a+2は4,1+2は3と表示される
  しかし\nを入れることによりprintf関数も改行できる
  普通に\を出力したい場合は\\と\を重ねて書く
 
  またputsは変数の内容が出力できないがprintfはできる
+
+ %d等はVariable.cの出力フォーマット指定子を参照
  */
